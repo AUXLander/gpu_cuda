0ef3292e6071713710c5a61ca219fa3f225a3a9e
Denis <36292357+AUXLander@users.noreply.github.com>
2022-11-23 09:41:14 +0300

    edit benchmark

--- axpy/benchmark.h
+++ axpy/benchmark.h
@@ -51,7 +51,7 @@ public:
         kernel<T>::finalize(size, a, x.data(), 1, y.data(), 1);
 
         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - begin);
-		auto time_ms  = static_cast<double>(duration.count()) / 1000.0;
+        auto time_ms  = static_cast<double>(duration.count()) / 1000.0;
 
         return time_ms / static_cast<double>(number_of_tests);
     }
